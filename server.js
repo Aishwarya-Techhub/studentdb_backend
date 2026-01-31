57265d6a1298ca462b10906dedb39d26d49f3b79
Aishwarya <rraishwarya5@gmail.com>
2026-01-31 15:47:39 +0530

    Added authentication

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
+require('dotenv').config();
+
 const express=require('express');
 const app=express();
 
-require('dotenv').config(); 
 require('./data/mongo');
 
 app.use(express.json());
+const jwt=require('jsonwebtoken');
 
-const studentRoutes=require('./router/student.route');
-app.use('/students',studentRoutes);
+app.get('/profile',(req,res)=>
+{
+    const authHeader=req.headers.authorization;
+    
+    if(!authHeader)
+    {
+        return res.status(401).json({message:"Token missing"});
+    }
+    const token=authHeader.split(' ')[1];
+    try
+    {
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
+        res.json({
+            message:"Access granted",
+            user:decoded
+        });
+    }
+    catch(err)
+    {
+        res.status(401).json({
+            message:"Invalid token"
+        });
+    }
 
-app.listen(5000,()=>{
-    console.log("server is running on http://localhost:5000");
 });
 
+const authRoute=require('./router/auth.route');
+app.use('/auth',authRoute);
 
+const studentRoute=require('./router/student.route');
+app.use('/students',studentRoute);
 
+app.listen(5000,()=>
+{
+    console.log("Server is running on http://localhost:5000");
+});
